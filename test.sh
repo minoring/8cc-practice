5d227dfc2d3d73e11ebc05262f718a8eb5ab0d15
minoring <minho1350@gmail.com>
2020-03-23 20:59:39 -0700

    test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 function test {
-  expected=$1
-  expr=$2
+  expected="$1"
+  expr="$2"
 
-  echo $expr | ./8cc > tmp.s
-  echo $?
+  echo "$expr" | ./8cc > tmp.s
   if [ ! $? ]; then
     echo "Failed to compile $expr"
     exit
   fi
   gcc -o tmp.out driver.c tmp.s || exit
   result="`./tmp.out`"
-  if [ $result != $expected ]; then
+  if [ "$result" != "$expected" ]; then
     echo "Test failed: $expected expected but got $result"
     exit
   fi
